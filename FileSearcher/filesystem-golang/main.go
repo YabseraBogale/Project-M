b93d733e4da58c9d44088395901050e1898c6aaf
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-20 17:17:13 +0300

    ok

--- FileSearcher/filesystem-golang/main.go
+++ FileSearcher/filesystem-golang/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	print("Enter File To Search: ")
+	var input string
+	fmt.Scanln(&input)
+	fileNAME := "./" + input
+	fileinfo, err := os.Stat(fileNAME)
+	if err != nil {
+
+	}
+	println(fileinfo)
 
 }
