81c96e8a7f8920395ff1c47e1698e673d1ece3f7
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-20 18:46:38 +0300

    ok

--- FileSearcher/filesystem-c++/main.cpp
+++ FileSearcher/filesystem-c++/main.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 #include<filesystem>
-namespace fs =std::filesystem;
-
+#include<fstream>
 int main(){
+    std::string filename="Readme.md";
+    //std::getline(std::cin,filename);
+    bool exist=std::filesystem::exists(filename);
+    if(exist==true){
+        std::cout<<std::filesystem::current_path()<<std::endl;
+    } else{
+        
+    }
     return 0;
 }
