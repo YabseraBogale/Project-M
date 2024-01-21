384332f9d6881adff565816804e186346fd2c6f0
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-21 18:08:04 +0300

    ok

--- FileSearcher/filesystem-c#/Program.cs
+++ FileSearcher/filesystem-c#/Program.cs
@@ -1,11 +1,13 @@
 ﻿static bool Finder(string? name){
     if(File.Exists(String.Format($"{name}"))){
         return true;
-    } else if(Directory.EnumerateDirectories(String.Format($"./{name}")).Any()){
-        IEnumerable<string> ListOfDir=Directory.EnumerateDirectories(String.Format($"./{name}"));
+    } else if(Directory.EnumerateDirectories(".").Any()){
+        IEnumerable<string> ListOfDir=Directory.EnumerateDirectories(".");
         foreach(string i in ListOfDir){
+            if(Finder(i)==true){
+                return true;
+            }
             
-            Console.WriteLine(String.Format($"{i}/{name}"));
         }
     }
     return false;
