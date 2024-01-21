a63a09149768c041bbfba61a2ae4975f6d525399
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-21 17:02:58 +0300

    ok

--- FileSearcher/filesystem-c#/Program.cs
+++ FileSearcher/filesystem-c#/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string Finder(string name){
+    string path=String.Format($"./{name}");
+    if(File.Exists(path)){
+        return path;
+    } else{
+        IEnumerable<string> ListOfDir=Directory.EnumerateDirectories(path);
+        int OuterBound=ListOfDir.Count();
+        while(OuterBound>0){
+            OuterBound-=1;
+        }
+    }
+}
+
+
+
+
+
+
+
+Console.Write("Enter File Name: ");
+string? name = Console.ReadLine();
+
+
