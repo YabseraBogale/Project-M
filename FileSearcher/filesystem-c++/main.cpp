ebc6dcd139777ceefedd7e714b8fe530052e8ed4
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-22 14:49:14 +0300

    ok

--- FileSearcher/filesystem-c++/main.cpp
+++ FileSearcher/filesystem-c++/main.cpp
@@ -2,6 +2,8 @@
 #include<filesystem>
 int main(){
     std::string filename="Hello World.txt";
-    
+    for(auto &i: std::filesystem::directory_iterator(".")){
+        std::cout<<i.is_directory()<<std::endl;
+    }
     return 0;
 }
