a51fba103e8648b8e26342fcc2434f1816c496b8
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-22 14:41:31 +0300

    ok

--- FileSearcher/filesystem-c++/main.cpp
+++ FileSearcher/filesystem-c++/main.cpp
@@ -1,15 +1,7 @@
 #include<iostream>
 #include<filesystem>
 int main(){
-    std::string filename="Readme.md";
-    //std::getline(std::cin,filename);
-    bool exist=std::filesystem::exists(filename);
-    if(exist==true){
-        
-    } else{
-        for(auto &dir: std::filesystem::directory_iterator("..")){
-                std::cout<<dir.path()<<std::endl;
-        }
-    }
+    std::string filename="Hello World.txt";
+    
     return 0;
 }
