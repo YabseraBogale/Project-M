ae38408b57def2ec6897c7ed0af0a34bd0e7f190
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-26 19:07:35 +0300

    ok

--- FileSearcher/filesystem-golang/main.go
+++ FileSearcher/filesystem-golang/main.go
@@ -1,35 +1,5 @@
 package main
 
-import "io/ioutil"
-
 func main() {
-	file, err := ioutil.ReadDir("./Folder")
-	if err != nil {
 
-	}
-	for _, i := range file {
-		println(i.Name())
-		println(List((i.Name())))
-	}
-
-}
-
-func List(name string) string {
-	if name == "done" {
-		println(name)
-		return "done"
-	}
-	file, err := ioutil.ReadDir(name)
-	if err != nil {
-		println("Error")
-	}
-
-	for _, i := range file {
-		if i.IsDir() == true {
-			println(i.Name())
-			return List("./" + i.Name())
-		}
-	}
-	println(name)
-	return "done"
 }
