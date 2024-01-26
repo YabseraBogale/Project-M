699673c804af14ad0021b19d8f290811170ff746
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-26 19:10:00 +0300

    ok

--- FileSearcher/filesystem-golang/main.go
+++ FileSearcher/filesystem-golang/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"io/ioutil"
+	"path"
+)
+
 func main() {
+
+	file, err := ioutil.ReadDir(".")
+	if err != nil {
+
+	}
+	for _, i := range file {
+		println(path.Dir(i.Name()))
+	}
 
 }
