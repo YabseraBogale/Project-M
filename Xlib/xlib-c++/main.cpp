b58efa9bfdfdbf26c32dd6175178fb70e6fcbc8e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-01 17:24:04 +0300

    ok

---
+++ Xlib/xlib-c++/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+ 
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/XKBlib.h>
+#include <X11/Xatom.h>
+#include <X11/keysym.h>
+ 
+int main()
+{
+    
+    return 0;
+}
