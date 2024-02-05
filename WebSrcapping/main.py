bfcb1fa08a2b7caa678338d03059d5915bbccdb7
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-05 20:53:54 +0300

    ok

--- WebSrcapping/main.py
+++ WebSrcapping/main.py
@@ -4,3 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+data_url_example = 'https://datahub.io/core/natural-gas/datapackage.json'
+
+# to load Data Package into storage
+package = datapackage.Package(data_url_example)
+
+# to load only tabular data
+resources = package.resources
+for resource in resources:
+    if resource.tabular:
+        data = pd.read_csv(resource.descriptor['path'])
+        print (data)
