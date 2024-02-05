040a0872ee677aa2ce2e539bdeeaab5588bcccd0
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-05 20:53:32 +0300

    ok

--- WebSrcapping/main.py
+++ WebSrcapping/main.py
@@ -1,26 +1,6 @@
-import requests
-from bs4 import BeautifulSoup as bts
-import pandas as pd
-import re
+import statsmodels.api as sm
+import datapackage
+import matplotlib.pyplot as plt
 import numpy as np
-from pytictoc import TicToc
-from datetime import datetime
-import time
-
-def getAndParseURL(url):
-    result = requests.get(url, headers={"User-Agent":"Chrome/103.0.0.0"}) # Safari/537.36. Mozilla/5.0
-    soup = bts(result.text, "html.parser")
-    return soup
-
-pages = [] #annual bestsellers&literary fiction
-for page in range(1,24):
-    pages.append("https://www.kitapyurdu.com/index.php?route=product/best_sellers&page="+str(page)+"&list_id=18&filter_in_stock=1&filter_in_stock=1")
-
-
-
-
-products = []
-for page in pages:
-    html = getAndParseURL(page)
-    for product in html.findAll("div",{"class":"cover"}):
-        products.append(product.a.get("href"))
+import pandas as pd
+import seaborn as sns
