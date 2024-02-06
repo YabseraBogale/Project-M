219d61ebb6cd970dd66ccd9b2cd98342547ce68d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-06 18:50:35 +0300

    ok

--- WebScoket/app.py
+++ WebScoket/app.py
@@ -7,11 +7,17 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-@app.route('')
+@app.route('/')
 def main():
     return render_template('base.html')
 
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
 
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
 
 if __name__=="__main__":
     socketio.run(app)
