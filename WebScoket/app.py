e3050bc78693b9f4ffcf6525d576a8363a36e545
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-06 18:48:25 +0300

    ok

--- WebScoket/app.py
+++ WebScoket/app.py
@@ -1,20 +1,17 @@
 from flask import Flask,render_template,request
 from flask_socketio import SocketIO, emit
-import subprocess
-
-app = Flask(__name__)
-socketio = SocketIO(app,debug=True,cors_allowed_origins='*',async_mode='eventlet')
 
 
-@app.route('/home')
+app=Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('')
 def main():
-        return render_template('base.html')
+    return render_template('base.html')
 
-@socketio.on("my_event")
-def checkping():
-    for x in range(5):
-        cmd = 'ping -c 1 8.8.8.8|head -2|tail -1'
-        listing1 = subprocess.run(cmd,stdout=subprocess.PIPE,text=True,shell=True)
-        sid = request.sid
-        emit('server', {"data1":x, "data":listing1.stdout}, room=sid)
-        socketio.sleep(1)
+
+
+if __name__=="__main__":
+    socketio.run(app)
