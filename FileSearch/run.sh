4131667191d3d64f9f9581ac94dc15ece1b2533a
YabseraBogale <yabserapython@gmail.com>
2024-09-11 10:53:58 +0300

    ok

--- FileSearch/run.sh
+++ FileSearch/run.sh
@@ -1,2 +1,3 @@
 git add . && git commit -m "ok" && git push
-x86_64-w64-mingw32-g++ main.cpp -static-libstdc++ -static-libgcc -o main && wine ./main.exe
+# x86_64-w64-mingw32-g++ main.cpp -static-libstdc++ -static-libgcc -o main && wine ./main.exe
+g++ -o main main.cpp && ./main.exe
