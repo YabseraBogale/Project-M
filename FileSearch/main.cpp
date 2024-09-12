7c7d38f1322ec3c8039fedd4514cc69e1cb057cb
YabseraBogale <yabserapython@gmail.com>
2024-09-12 13:53:25 +0300

    ok

--- FileSearch/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<filesystem>
-
-int main(){
-    auto pwd=std::filesystem::current_path();     
-    std::cout<<pwd<<std::endl;
-    return 0;
-}
-
--- FileSearch/run.sh
+++
@@ -1,3 +0,0 @@
-git add . && git commit -m "ok" && git push
-# x86_64-w64-mingw32-g++ main.cpp -static-libstdc++ -static-libgcc -o main && wine ./main.exe
-g++ -o main main.cpp && ./main
