5f743fa2bbe6368f917dfd91dd8b3431a0b55835
YabseraBogale <yabserapython@gmail.com>
2024-09-13 16:35:14 +0300

    ok

---
+++ filesearch/lib/filesearch.ex
@@ -0,0 +1,18 @@
+defmodule Filesearch do
+  @moduledoc """
+  Documentation for `Filesearch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Filesearch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ filesearch/test/filesearch_test.exs
@@ -0,0 +1,8 @@
+defmodule FilesearchTest do
+  use ExUnit.Case
+  doctest Filesearch
+
+  test "greets the world" do
+    assert Filesearch.hello() == :world
+  end
+end
