c36aa9e252eee17311b126acb70097b90307807e
YabseraBogale <yabserapython@gmail.com>
2024-09-24 20:23:36 +0300

    ko

--- golab/talktojs/main.go
+++ golab/talktojs/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/hello" {
+		switch r.Method {
+		case "POST":
+			if err := r.ParseForm(); err != nil {
+				fmt.Fprintf(w, "ParseForm")
+				return
+			}
+			fmt.Fprintf(w, "post from website\n")
+			username := r.PathValue("Username")
+			fmt.Fprintf(w, "Name = %s\n", username)
+		default:
+			fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+
+		}
+	}
+}
+
 func main() {
 	f := http.FileServer(http.Dir("./static"))
 	http.Handle("/", f)
+	http.HandleFunc("/hello", hello)
 	log.Print("Listening on :3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
