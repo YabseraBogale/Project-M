dc0481284216155754a9c8ccb24474f4f6b0ce02
YabseraBogale <yabserapython@gmail.com>
2024-09-24 19:34:00 +0300

    ko

--- golab/talktojs/main.go
+++ golab/talktojs/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
-func HelloJavascript(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-}
-
 func main() {
-	http.HandleFunc("/", HelloJavascript)
-	http.ListenAndServe(":8000", nil)
+	f := http.FileServer(http.Dir("./static"))
+	http.Handle("/", f)
+	log.Print("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
