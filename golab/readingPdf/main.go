d34ed45e3568d5d1f88332509f7f23f794be142c
YabseraBogale <yabserapython@gmail.com>
2024-09-25 16:45:46 +0300

    ok

--- golab/readingPdf/main.go
+++ golab/readingPdf/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.ReadFile("sample.pdf")
+	if err != nil {
+		log.Println(err)
+	}
+	for _, i := range file {
+		fmt.Println(i)
+	}
 }
