9db95cd79be1e984deee9357e69e057c5e421fc1
YabseraBogale <yabserapython@gmail.com>
2024-09-25 22:28:16 +0300

    ok

--- golab/readingPdf/main.go
+++ golab/readingPdf/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
+	"compress/zlib"
 	"fmt"
 	"log"
 	"os"
 	"strings"
-	"unicode/utf8"
 )
 
 func main() {
@@ -16,13 +16,28 @@ func main() {
 	}
 	scanner := bufio.NewScanner(strings.NewReader(string(file)))
 	count := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		if line == "stream" {
 			count = 1
 		} else if count == 1 && line != "endstream" {
-			fmt.Println(utf8.DecodeLastRuneInString(line))
+			r, err := zlib.NewReader(strings.NewReader(line))
+			if err != nil {
+				println("here")
+				log.Println(err)
+			}
+			b := make([]byte, len(line))
+			for {
+				n, err := r.Read(b)
+				fmt.Println(string(b[:n]))
+				if err != nil {
+					return
+				}
+
+			}
+
 		} else if line == "endstream" {
 			count = 0
 		}
