7e3c326aa8c4e8016e22a7227a84e38a1a586cea
YabseraBogale <yabserapython@gmail.com>
2024-09-25 19:01:19 +0300

    ok

--- golab/readingPdf/main.go
+++ golab/readingPdf/main.go
@@ -14,13 +14,22 @@ func main() {
 		log.Println(err)
 	}
 	scanner := bufio.NewReader(strings.NewReader(string(file)))
+	var data []byte
+	count := 0
 	for i := 0; i < len(file); i++ {
 		line, _, err := scanner.ReadLine()
 		if err != nil {
 			log.Println(err)
 			break
 		}
-		fmt.Println(string(line))
+		if string(line) == "stream" {
+			count = 1
+		} else if count == 1 && string(line) != "endstream" {
+			data = append(data, line...)
+		} else if string(line) == "endstream" {
+			count = 0
+		}
 
 	}
+	fmt.Println(string(data))
 }
