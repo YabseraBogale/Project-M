1f127ae78148ead381df50ca377b88e0158a2638
YabseraBogale <yabserapython@gmail.com>
2024-09-26 11:53:11 +0300

    it seems ok

--- golab/readingPdf/main.go
+++ golab/readingPdf/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"compress/zlib"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -15,22 +16,21 @@ func main() {
 	}
 	scanner := bufio.NewScanner(strings.NewReader(string(file)))
 	count := 0
-	data := [][]byte{}
 	for scanner.Scan() {
-		line := scanner.Text()
 
+		line := scanner.Text()
 		if line == "stream" {
 			count = 1
 		} else if count == 1 && line != "endstream" {
 			r, err := zlib.NewReader(strings.NewReader(line))
 			if err != nil {
-				println("here")
 				log.Println(err)
 			}
 			b := make([]byte, len(line))
 			for {
 				n, err := r.Read(b)
-				data = append(data, b[:n])
+				fmt.Println(string(b[:n]))
+
 				if err != nil {
 					return
 				}
@@ -38,11 +38,10 @@ func main() {
 			}
 
 		} else if line == "endstream" {
+
 			count = 0
 		}
 
 	}
-	os.WriteFile("data.txt", data[0], 0777)
-	os.WriteFile("data.txt", data[1], 0777)
-	os.WriteFile("data.txt", data[2], 0777)
+
 }
