ac70abc8c91df3d42d3f8cbbc27e4310b9247bbe
YabseraBogale <yabserapython@gmail.com>
2024-09-26 10:27:11 +0300

    it seems ok

--- golab/readingPdf/main.py
+++ golab/readingPdf/main.py
@@ -18,7 +18,7 @@ stream = re.compile(rb'.*?FlateDecode.*?stream(.*?)endstream', re.S)
 for s in stream.findall(pdf):
     s = s.strip(b'\r\n')
     try:
-        print(zlib.decompress(s))
-        print("")
+        print(zlib.decompress(s).decode("utf-8"))
+        print("\n")
     except:
         pass
