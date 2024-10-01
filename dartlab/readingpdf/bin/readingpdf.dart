eab2fc1d74c96393b6db116788c320fe18e6c222
YabseraBogale <yabserapython@gmail.com>
2024-10-01 12:25:41 +0300

    ok

--- dartlab/readingpdf/bin/readingpdf.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:ffi';
-import 'dart:io';
-
-void main() async {
-  final file = File("sample.pdf");
-}
--- dartlab/readingpdf/test/readingpdf_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:readingpdf/readingpdf.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
