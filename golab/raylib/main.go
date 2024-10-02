4e148b680f61407af4e0ede6803effa47daa6d44
YabseraBogale <yabserapython@gmail.com>
2024-10-02 20:13:01 +0300

    fuck this shit skill issue

--- golab/raylib/main.go
+++ golab/raylib/main.go
@@ -1,19 +1,37 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"image/color"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+var (
+	xPosition int32   = 400
+	yPosition int32   = 250
+	radius    float32 = 20
+)
 
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	rl.InitWindow(800, 500, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(60)
+	rl.SetTargetFPS(80)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
+		rl.DrawCircle(xPosition, yPosition, radius, color.RGBA{0x34, 0xeb, 0x64, 0xff})
+		if rl.IsKeyPressed(rl.KeyUp) {
+			yPosition -= 10
+		} else if rl.IsKeyPressed(rl.KeyDown) {
+			yPosition += 10
+		} else if rl.IsKeyPressed(rl.KeyRight) {
+			xPosition += 10
+		} else if rl.IsKeyPressed(rl.KeyLeft) {
+			xPosition -= 10
+		}
 		rl.EndDrawing()
+
 	}
+
 }
