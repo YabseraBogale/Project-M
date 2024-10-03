32c288303c2af9feb753455f849e8ed26a33c42c
YabseraBogale <yabserapython@gmail.com>
2024-10-03 20:45:50 +0300

    ok

--- golab/readingPdf/main.go
+++ golab/readingPdf/main.go
@@ -31,7 +31,7 @@ func main() {
 			b := make([]byte, len(line))
 			for {
 				n, err := r.Read(b)
-				fmt.Println(strings.Split(string(b[:n]), "56.8 704.989 Td /F1 12 Tf"))
+				fmt.Println(string(b[:n]))
 				if err != nil {
 					return
 				}
