02cc951b2742f29243434951984dec5f2ad8a984
YabseraBogale <yabserapython@.com>
2024-10-04 16:26:20 +0300

    ok

--- golab/decode/main.go
+++ golab/decode/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"compress/zlib"
 	"fmt"
-	"log"
 	"strings"
+	"unicode/utf8"
 )
 
 func main() {
@@ -13,22 +11,11 @@ func main() {
 	dataFromPdf := "<20>5<150B090507>1<0804>-6<07>1<0905150A07>1<0E090507>1<0B>-8<0903>1<1102>2<07>1<0B>-8<091502>2<0507>1<0B0919081517>-2<08090E07>1<1107>1<0B09050D>2<0C>-2<160921>-1<150B0904>1<15120802>2<14>-7<090815>8<17>-2<090C>-2<0D>2<0C>-2<02>2<07>1<0B>-8<0907>1<081003>1<1007>1<090503>1<0E>-7<090C>6<07>1<03>1<0C>-2<1508091307>1<02>-5<02>2<07>1<0B>-8<09>"
 	data := [][]byte{}
 	for _, i := range strings.Split(strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(dataFromPdf, "<", ""), ">", ""), "-", " "), " ") {
+
 		data = append(data, HexaToBinary(i))
 	}
 	result := ReduceByteArray(data)
-	r, err := zlib.NewReader(bytes.NewReader(result))
-	if err != nil {
-		log.Println("first log", err)
-	}
-
-	for {
-		n, err := r.Read(result)
-		fmt.Println(result[:n])
-		if err != nil {
-			log.Println("second log", err)
-		}
-	}
-
+	fmt.Println(utf8.DecodeRune(result))
 }
 
 func HexaToBinary(number string) []byte {
