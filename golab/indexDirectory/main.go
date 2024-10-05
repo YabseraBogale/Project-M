cdfb129a7a6f3b107de017849619b65698325a0a
YabseraBogale <yabserapython@.com>
2024-10-05 12:06:48 +0300

    ok

--- golab/indexDirectory/main.go
+++ golab/indexDirectory/main.go
@@ -2,46 +2,21 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
+	"log"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
+	url := "http://maslovd.ru/public/mp3/Muse/Albums%20(CD)/?C=N;O=D"
+	webscraper := colly.NewCollector()
 
-	fileUrl := "http://maslovd.ru/public/mp3/Muse/Albums%20(CD)/2022%20-%20Will%20Of%20The%20People/06%20You%20Make%20Me%20Feel%20Like%20It's%20Halloween.mp3"
+	webscraper.OnRequest(func(r *colly.Request) {
+		fmt.Println(r.URL)
+	})
 
-	// Download the file, params:
-	// 1) name of file to save as
-	// 2) URL to download FROM
-	err := DownloadFile("saveas.mp3", fileUrl)
+	err := webscraper.Visit(url)
 	if err != nil {
-		fmt.Println("Error downloading file: ", err)
-		return
+		log.Println(err)
 	}
-
-	fmt.Println("Downloaded: " + fileUrl)
-}
-
-// DownloadFile will download from a given url to a file. It will
-// write as it downloads (useful for large files).
-func DownloadFile(filepath string, url string) error {
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
 }
