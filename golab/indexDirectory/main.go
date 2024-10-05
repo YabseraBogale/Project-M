c01c1ee1a09b4355ce196d28bfd670d489a58eb3
YabseraBogale <yabserapython@.com>
2024-10-05 13:34:00 +0300

    ok

--- golab/indexDirectory/main.go
+++ golab/indexDirectory/main.go
@@ -3,20 +3,41 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
 	url := "http://maslovd.ru/public/mp3/Muse/Albums%20(CD)/?C=N;O=D"
+	album := "http://maslovd.ru/public/mp3/Muse/Albums%20(CD)"
 	webscraper := colly.NewCollector()
-
-	webscraper.OnRequest(func(r *colly.Request) {
-		fmt.Println(r.URL)
+	data := []string{}
+	webscraper.OnHTML("td", func(h *colly.HTMLElement) {
+		a := h.ChildAttr("a", "href")
+		if a != "" {
+			data = append(data, album+"/"+a)
+		}
 	})
-
 	err := webscraper.Visit(url)
 	if err != nil {
 		log.Println(err)
+	}
+
+	for _, i := range data[1:] {
+		var wait sync.WaitGroup
+		go func(src string) {
+			wait.Wait()
+			muses := colly.NewCollector()
+			muses.OnHTML("td", func(h *colly.HTMLElement) {
+				fmt.Println(h.ChildAttr("a", "href"))
+			})
+			err := muses.Visit(src)
+			if err != nil {
+				log.Println(err)
+			}
+		}(i)
+		wait.Done()
+
 	}
 }
