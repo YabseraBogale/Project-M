4ff378bdee995a958bfd975079b31319bbfea99b
YabseraBogale <yabserapython@gmail.com>
2024-10-08 17:53:32 +0300

    ok

--- golab/letsChat/main.go
+++ golab/letsChat/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.FormValue("message"))
+	})
 	http.ListenAndServe(":8080", nil)
 
 }
