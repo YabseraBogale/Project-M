07b612bb964dc7dabe316ed5238b29365b307320
YabseraBogale <yabserapython@.com>
2024-10-12 12:44:11 +0300

    golang in server is still not good for dev exp

--- golab/letsChat/main.go
+++ golab/letsChat/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Template struct {
+	template *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.template.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World")
+	render := &Template{
+		template: template.Must(template.ParseGlob("public/index.html")),
+	}
+	e.Renderer = render
+	e.POST("/", func(c echo.Context) error {
+		name := c.FormValue("message")
+		fmt.Println(name)
+		return c.Render(http.StatusOK, "index.html", nil)
 	})
 
 	e.Logger.Fatal(e.Start("127.0.0.1:8080"))
