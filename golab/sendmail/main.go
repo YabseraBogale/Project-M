2fc0b1ed965d2ef81b6cd798df1b0accc64e8509
YabseraBogale <yabserapython@gmail.com>
2024-10-20 15:20:41 +0300

    doing skeleton for both login and regsister

--- golab/sendmail/main.go
+++ golab/sendmail/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/smtp"
+	"os"
+)
 
+func main() {
+	smtphost := "smtp.gmail.com"
+	smtpport := "587"
+	email := "yabserapython@gmail.com"
+	to := []string{"yabserapython@gmail.com"}
+	app_id := os.Getenv("app_id")
+	msg := "subject: This is to test golang\n\nthis the body to the message"
+
+	message := []byte(msg)
+	auth := smtp.PlainAuth("", email, app_id, smtphost)
+	err := smtp.SendMail(smtphost+":"+smtpport, auth, email, to, message)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
