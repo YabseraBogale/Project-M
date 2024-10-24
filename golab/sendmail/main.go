bb298f1d827818534529095b0c11e5463e9993a0
YabseraBogale <yabserapython@gmail.com>
2024-10-24 18:49:30 +0300

    basic skeleton done ?

--- golab/sendmail/main.go
+++ golab/sendmail/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"html/template"
 	"log"
 	"net/smtp"
 	"os"
 )
+
+func GetHtml() (string, error) {
+	temp, err := template.ParseFiles("index.html")
+	if err != nil {
+		return "", err
+	}
+	f := []string{}
+	buf := new(bytes.Buffer)
+	if err = temp.Execute(buf, f); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
 
 func main() {
 	smtphost := "smtp.gmail.com"
