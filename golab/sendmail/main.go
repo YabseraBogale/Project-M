19fcd725d00a45bdb79994c8071a243b05b29c9f
YabseraBogale <yabserapython@gmail.com>
2024-10-27 10:13:22 +0300

    ok

--- golab/sendmail/main.go
+++ golab/sendmail/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"net/smtp"
 )
 
 func GetHtml() (string, error) {
 	temp, err := template.ParseFiles("index.html")
 	if err != nil {
+		log.Println(err)
 		return "", err
 	}
 	f := []string{"Hello World"}
@@ -21,9 +23,35 @@ func GetHtml() (string, error) {
 }
 
 func main() {
+
 	str, err := GetHtml()
 	if err != nil {
 		log.Fatalln(err)
 	}
+	from := "cheretaaddis@gmail.com"
+	password := "zgzd xtlt emlc tzfb"
+	to := []string{"yabserapython@gmail.com"}
+
+	// SMTP server configuration
+	smtpHost := "smtp.example.com" // Example: smtp.gmail.com
+	smtpPort := "587"              // For SSL: 465, for STARTTLS: 587
+
+	// Message
+	subject := "Subject: Test HTML Email\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+
+	// Combine headers and body
+	message := []byte(subject + mime + str)
+
+	// Authentication
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Send Email
+	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		fmt.Println("Error sending email:", err)
+		return
+	}
+	fmt.Println("Email sent successfully!")
 	fmt.Println(str)
 }
