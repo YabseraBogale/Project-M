395da227ef4693c749721e1f7780ad90e4561d0f
YabseraBogale <yabserapython@gmail.com>
2024-10-31 18:17:25 +0300

    ok

--- linkedin/main.go
+++ linkedin/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -11,34 +15,26 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-	var input string
-	for _, j := range file {
 
-		fmt.Println(file[j])
-		fmt.Scanln(&input)
-		if input == "q" {
+	scanner := bufio.NewScanner(bytes.NewBuffer(file))
+	count := 0
+	slower := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		slower += 1
+		fmt.Println(line)
+		if strings.Contains(line, "EmailAddress") {
+			count += 1
+			fmt.Println(line)
+		}
+		if slower%100 == 0 {
+			println("At line", slower)
+			time.Sleep(30 * time.Second)
+		}
+		if slower == 2 {
 			break
 		}
+
 	}
-	// scanner := bufio.NewScanner(bytes.NewBuffer(file))
-	// count := 0
-	// slower := 1
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// 	slower += 1
-
-	// 	if strings.Contains(line, "EmailAddress") {
-	// 		count += 1
-	// 		fmt.Println(line)
-	// 	}
-	// 	if slower%100 == 0 {
-	// 		println("At line", slower)
-	// 		time.Sleep(30 * time.Second)
-	// 	}
-	// 	if slower == 10000 {
-	// 		break
-	// 	}
-
-	// }
-	// println("found", count, "email")
+	println("found", count, "email")
 }
