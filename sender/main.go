17f61c67f4d014955e5153ab92f189d4a5de759f
YabseraBogale <yabserapython@gmail.com>
2024-11-06 11:47:21 +0300

    ok

--- sender/main.go
+++ sender/main.go
@@ -23,6 +23,7 @@ func main() {
 	Germany := 0
 	Saudi := 0
 	Angola := 0
+	USA := 0
 	Unknown := 0
 	count := 0
 
@@ -41,6 +42,8 @@ func main() {
 			Saudi += 1
 		} else if Country == "Angola" {
 			Angola += 1
+		} else if Country == "USA" {
+			USA += 1
 		} else {
 			Unknown += 1
 		}
@@ -51,22 +54,24 @@ func main() {
 	germany := float32((Germany * 100)) / float32(count)
 	saudi := float32((Saudi * 100)) / float32(count)
 	angola := float32((Angola * 100)) / float32(count)
+	usa := float32((USA * 100)) / float32(count)
 	unknown := float32((Unknown * 100)) / float32(count)
 	ExpectedPeopleToSeeMin := count * 17 / 100
 	ExpectedPeopleToSeeMax := count * 25 / 100
 	ExpectedPeopleToSeeCallMin := ExpectedPeopleToSeeMin * 17 / 100
 	ExpectedPeopleToSeeCallMax := ExpectedPeopleToSeeMin * 25 / 100
-	DataSent := (10 * count) / 1000.0
+	DataSent := float32(10*count) / float32(1000000)
 	fmt.Println("All Assumations being all the email works")
 	fmt.Println("Indonesia:", Indonesia, "Percentage:", indonesia)
 	fmt.Println("Germany:", Germany, "Percentage:", germany)
 	fmt.Println("Saudi:", Saudi, "Percentage:", saudi)
 	fmt.Println("Angola:", Angola, "Percentage:", angola)
+	fmt.Println("USA:", USA, "Percentage:", usa)
 	fmt.Println("Unknown:", Unknown, "Percentage:", unknown)
 	fmt.Println("Expected 17% number of people to see the email", ExpectedPeopleToSeeMin)
 	fmt.Println("Expected 25% number of people to see the email", ExpectedPeopleToSeeMax)
 	fmt.Println("Expected 17% number of people to call", ExpectedPeopleToSeeCallMin)
 	fmt.Println("Expected 25% number of people to call", ExpectedPeopleToSeeCallMax)
-	fmt.Println("Size of data sent over email for", count, "is", DataSent, "mb")
+	fmt.Println("Size of data sent over email for", count, "is", DataSent, "gb")
 
 }
