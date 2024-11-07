8bd1a0dfcf3bd0fc6efc7451abdffe5d40bfecef
YabseraBogale <yabserapython@gmail.com>
2024-11-07 19:02:42 +0300

    ok

--- linkedin/database.sql
+++ linkedin/database.sql
@@ -5,3 +5,12 @@ create table UserEmail(
     Email varchar(30) not null,
     HQCompanyName varchar(30) not null
 );
+
+create table UserData(
+	Email varchar(30) not null primary key,
+    FirstName varchar(30) not null,
+    LastName varchar(30) not null,
+    Country varchar(30) not null,
+    HQCompanyName varchar(30) not null,
+	Sent varchar(30) not null 
+);
