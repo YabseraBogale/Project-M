dd9ed675384cea5c2b05ff6ecfd0beba7e72eabd
YabseraBogale <yabserapython@gmail.com>
2024-11-07 19:19:19 +0300

    ok

---
+++ postgres/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const( 
+	host ="localhost"
+ 	port =5432
+	password=os.Getenv("dbpassword")
+	dbname="Gold"
+)
+
+func main(){
+	sqlite,err:=sql.Open("sqlite3","gold.db")
+	if err!=nil{
+		log.Println(err)
+	}
+	defer sqlite.Close()
+	postgres,err:=sql.Open("postgres",fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
+	if err!=nil{
+		log.Println(err)
+	}
+	defere postgres.Close()
+	
+}
