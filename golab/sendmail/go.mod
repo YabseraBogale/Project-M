dd02dd5c820618c21f6ed662d3122b9a7404b55d
YabseraBogale <yabserapython@gmail.com>
2024-11-07 09:17:47 +0300

    ok

--- golab/sendmail/go.mod
+++ golab/sendmail/go.mod
@@ -3,8 +3,14 @@ module github.com/YabseraBogale/Project-M/golab/sendmail
 go 1.23.2
 
 require (
+	github.com/emersion/go-imap v1.2.1
+	github.com/emersion/go-message v0.18.1
 	github.com/mattn/go-sqlite3 v1.14.24
 	gopkg.in/mail.v2 v2.3.1
 )
 
-require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+require (
+	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
--- golab/sendmail/main.go
+++ golab/sendmail/main.go
@@ -8,6 +8,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+	go_mail "github.com/emersion/go-message/mail"
 	_ "github.com/mattn/go-sqlite3"
 	"gopkg.in/mail.v2"
 )
@@ -17,58 +20,94 @@ const from string = "cheretaaddis@gmail.com"
 const smtpHost = "smtp.gmail.com" // Example: smtp.gmail.com
 const smtpPort = 587
 
-var numberOfSent int = 400
 var password string = os.Getenv("password")
 
 func main() {
+
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Println(err)
+	}
+	log.SetOutput(file)
+
+	imapClient, err := client.DialTLS("imap.gmail.com:993", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.SetOutput(file)
+	defer imapClient.Logout()
+
+	if err := imapClient.Login(from, password); err != nil {
+		log.Println("Login failed:", err)
+	}
+
+	// If login is successful
+	fmt.Println("Logged in successfully!")
+
 	db, err := sql.Open("sqlite3", "gold.db")
 	if err != nil {
 		log.Println(err)
 	}
 	defer db.Close()
-	// 	select Email,Sent from userdata WHERE Country='Germany' and Sent='not_sent' LIMIT 50;
-	// 	select Email,Sent from userdata WHERE Country='Germany' and Sent='not_sent' LIMIT 50;
+	// 	select Email,Sent from userdata WHERE Country='Germany' and Sent='not_sent' LIMIT 50; -- sent
 	// 	select Email,Sent from userdata WHERE Country='Saudi' and Sent='not_sent' LIMIT 50;
 	// 	select Email,Sent from userdata WHERE Country='USA' and Sent='not_sent' LIMIT 50;
 	// 	select Email,Sent from userdata WHERE Country='Indonesia' and Sent='not_sent' LIMIT 50;
 	// 	select Email,Sent from userdata WHERE Country='Angola' and Sent='not_sent' LIMIT 50;
-	row, err := db.Query(`Select Email,Sent from userdata where Sent='not_sent';`)
+	row, err := db.Query(`Select Email From userdata WHERE Country='Angola' and Sent='not_sent' LIMIT 4;`)
 	if err != nil {
 		log.Println(err)
 	}
+	defer row.Close()
 
 	for row.Next() {
-		numberOfSent -= 1
-		if numberOfSent == 0 {
-			break
 
-		}
 		var Email string
-		var Sent string
-		row.Scan(&Email, &Sent)
+
+		row.Scan(&Email)
 
 		sent, err := EmailSender(Email)
 		if err != nil {
 			log.Println(err)
 		} else if sent {
-			statment, err := db.Prepare(`update userdata set Sent=(?) where Email=(?);`)
+			fmt.Println("Sent to...", Email)
+
+			mailbox, err := imapClient.Select("INBOX", false)
 			if err != nil {
 				log.Println(err)
 			}
-			Sent = "sent"
-			result, err := statment.Exec(Sent, Email)
-			if err != nil {
-				log.Println(err)
+			if mailbox.Messages > 0 {
+				seqset := new(imap.SeqSet)
+				seqset.AddRange(mailbox.Messages, mailbox.Messages)
+				messages := make(chan *imap.Message, 1)
+				section := &imap.BodySectionName{}
+				err = imapClient.Fetch(seqset, []imap.FetchItem{section.FetchItem()}, messages)
+				if err != nil {
+					log.Println(err)
+				}
+				msg := <-messages
+				if msg == nil {
+					log.Println("Server didn't return message")
+				}
+				r := msg.GetBody(section)
+				if r == nil {
+					log.Println("Server didn't return message body")
+				}
+				mr, err := go_mail.CreateReader(r)
+				if err != nil {
+					log.Fatal(err)
+				}
+				header := mr.Header
+				from, err := header.AddressList("From")
+				if err == nil {
+					fmt.Println("From:", from)
+				}
+				subject, err := header.Subject()
+				if err == nil {
+					fmt.Println("Subject:", subject)
+				}
 			}
-			fmt.Println("Sent to", Email, "with result", result)
 		}
 	}
-
 }
 
 func EmailSender(to string) (bool, error) {
