8f7f5ecde2aeaeaa8e17b5d812bc6956ebfa2714
YabseraBogale <yabserapython@gmail.com>
2024-11-07 19:31:36 +0300

    ok

--- postgres/main.go
+++ postgres/main.go
@@ -20,6 +20,12 @@ const (
 var password string = os.Getenv("dbpassword")
 
 func main() {
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Println(err)
+	}
+	log.SetOutput(file)
+
 	sqlite, err := sql.Open("sqlite3", "gold.db")
 	if err != nil {
 		log.Println(err)
