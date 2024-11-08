3bec87988193aff378de52f69e16ac9cb7cc028e
YabseraBogale <yabserapython@gmail.com>
2024-11-08 20:17:09 +0300

    ok going to get imap

--- reciver/main.go
+++ reciver/main.go
@@ -18,7 +18,7 @@ func main() {
 		log.Println(err)
 	}
 	log.SetOutput(file)
-	imapClient, err := client.DialTLS("imap.google.com", nil)
+	imapClient, err := client.DialTLS("imap.gmail.com:993", nil)
 	if err != nil {
 		log.Println(err)
 	}
@@ -33,6 +33,26 @@ func main() {
 		log.Println(err)
 	}
 	if inbox.Messages > 0 {
+		fmt.Println(inbox.Messages)
+		// seqset := new(imap.SeqSet)
+		// seqset.AddRange(inbox.Messages, inbox.Messages)
+		// messages := make(chan *imap.Message, 1)
+		// section := &imap.BodySectionName{}
+		// if err := imapClient.Fetch(seqset, []imap.FetchItem{section.FetchItem()}, messages); err != nil {
+		// 	log.Println(err)
+		// }
+		// msg:=<-messages
+		// if msg==nil{
+		// 	log.Println("Server didn't Messages")
+		// }
+		// r:=msg.GetBody(section)
+		// if r==nil{
+		// 	log.Println("Server didn't return message body")
+		// }
+		// ml,err:=mail.CreateReader(r)
+		// if err!=nil{
+		// 	log.Println(err)
+		// }
 
 	} else {
 		fmt.Println("No new Message")
