53ac8388b35210df7731bc08cd00983193ff94b9
YabseraBogale <yabserapython@gmail.com>
2024-11-09 20:39:24 +0300

    ok

--- pythonlab/sendandrecive/main.py
+++ pythonlab/sendandrecive/main.py
@@ -28,17 +28,7 @@ for i in mail_ids:
                 message = email.message_from_bytes(response_part[1])
                 mail_from = message['from']
                 mail_subject = message['subject']
-                if message.is_multipart():
-                    mail_content = ''
-
-                    for part in message.get_payload():
-                        if part.get_content_type() == 'text/plain':
-                            mail_content += part.get_payload()
-
-                else:
-                    mail_content = message.get_payload()
-                print(f'From: {mail_from}')
-                print(f'Subject: {mail_subject}')
-                print(f'Content: {mail_content}')
+                if mail_subject.find("Failure")>=1 or mail_subject.find("Delay")>=1:
+                    print(message['In-Reply-To'].replace("<","").replace(">","").split("."))
     except Exception as e:
         print(str(e))
