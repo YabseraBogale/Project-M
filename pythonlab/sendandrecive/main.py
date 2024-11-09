67ab61bfc7641936200e4c2eca0ec6686c5eca7b
YabseraBogale <yabserapython@gmail.com>
2024-11-09 18:24:52 +0300

    ok

--- pythonlab/sendandrecive/main.py
+++ pythonlab/sendandrecive/main.py
@@ -7,33 +7,38 @@ Email="cheretaaddis@gmail.com"
 password=os.getenv("password")
 server="imap.gmail.com"
 
-mail=imaplib.IMAP4_SSL(server)
+mail=imaplib.IMAP4_SSL(server,993)
 
 
 result=mail.login(Email,password)
 
 
 mail.select("inbox")
-status,data=mail.search("None","ALL")
+status,data=mail.search(None,"ALL")
 mail_ids = []
 for block in data:
-    mail_ids += block.split()
+    mail_ids+=block.split()
+
 
 for i in mail_ids:
-    status, data = mail.fetch(i, '(RFC822)')
-    for response_part in data:
-        if isinstance(response_part, tuple):
-            message = email.message_from_bytes(response_part[1])
-            mail_from = message['from']
-            mail_subject = message['subject']
-            if message.is_multipart():
-                mail_content = ''
+    try:    
+        status, data = mail.fetch(i, '(RFC822)')
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                message = email.message_from_bytes(response_part[1])
+                mail_from = message['from']
+                mail_subject = message['subject']
+                if message.is_multipart():
+                    mail_content = ''
 
-                for part in message.get_payload():
-                    if part.get_content_type() == 'text/plain':
-                        mail_content += part.get_payload()
-            else:
-                mail_content = message.get_payload()
-            print(f'From: {mail_from}')
-            print(f'Subject: {mail_subject}')
-            print(f'Content: {mail_content}')
+                    for part in message.get_payload():
+                        if part.get_content_type() == 'text/plain':
+                            mail_content += part.get_payload()
+
+                else:
+                    mail_content = message.get_payload()
+                print(f'From: {mail_from}')
+                print(f'Subject: {mail_subject}')
+                print(f'Content: {mail_content}')
+    except Exception as e:
+        print(str(e))
