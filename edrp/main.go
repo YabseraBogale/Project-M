a68d561fb9534b4cd883a998d02bb653fe81d689
YabseraBogale <yabserapython@gmail.com>
2024-12-01 17:27:00 +0300

    ok

--- edrp/main.go
+++ edrp/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"image/png"
 	"log"
-	"net/http"
 	"os"
+	"time"
 
 	"github.com/vova616/screenshot"
 )
@@ -16,24 +15,12 @@ func main() {
 		log.Println(err)
 	}
 	log.SetOutput(logfile)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	for {
 		img, err := screenshot.CaptureScreen()
 		if err != nil {
 			log.Println(err)
 		}
-		imgfile, err := os.Create("./file.png")
-		if err != nil {
-			log.Println(err)
-		}
-		defer imgfile.Close()
-		err = png.Encode(imgfile, img)
-		if err != nil {
-			log.Println(err)
-		}
-		fmt.Scanf("Helloworld", w)
-	})
-	err = http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		log.Println(err)
+		filename := fmt.Sprintf("%s.png", time.Now())
+		imgfile, err := os.Create("")
 	}
 }
