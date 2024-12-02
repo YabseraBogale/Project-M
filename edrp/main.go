aca0d49fee00bbc21c52a7a756d85e9acaecef9c
YabseraBogale <yabserapython@gmail.com>
2024-12-02 10:21:35 +0300

    :ok

--- edrp/main.go
+++ edrp/main.go
@@ -14,22 +14,39 @@ func main() {
 	logfile, err := os.OpenFile("err.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Println(err)
+		panic(err)
 	}
 	log.SetOutput(logfile)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	upload := fmt.Sprintf("%s/.uploads", home)
+	err = os.Mkdir(upload, 0666)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
 	for {
 		img, err := screenshot.CaptureScreen()
 		if err != nil {
 			log.Println(err)
+			panic(err)
 		}
-		filename := fmt.Sprintf("~./.uploads/.%s.png", time.Now())
-		imgfile, err := os.Create(filename)
+
+		filename := fmt.Sprintf("%s/.%s.png", upload, time.Now())
+		imgfile, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err != nil {
 			log.Println(err)
+			panic(err)
 		}
 		err = png.Encode(imgfile, img)
 		if err != nil {
 			log.Println(err)
+			panic(err)
 		}
 		time.Sleep(3 * time.Second)
+		imgfile.Close()
 	}
 }
