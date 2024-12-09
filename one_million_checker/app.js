a7ea0bdce0b3cd09132d973ac14f8632401190c5
YabseraBogale <yabserapython@gmail.com>
2024-12-09 21:46:48 +0300

    ok

--- one_million_checker/app.js
+++ one_million_checker/app.js
@@ -3,8 +3,7 @@ const ul=document.getElementById("ul")
 const frame=document.createDocumentFragment()
 for(let i=0;i<50;i++){
     const li=document.createElement("li")
-    const input=document.createElement("input")
-    li.innerHTML=input.checked
+    li.innerHTML="<input type='checkbox'>"
     frame.appendChild(li)
 }
 ul.appendChild(frame)
