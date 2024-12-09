b379fdb93451df614180c87037c439050d4fac49
YabseraBogale <yabserapython@gmail.com>
2024-12-09 22:09:35 +0300

    ok

--- one_million_checker/app.js
+++ one_million_checker/app.js
@@ -1,9 +1,13 @@
 const ul=document.getElementById("ul")
 
 const frame=document.createDocumentFragment()
-for(let i=0;i<50;i++){
-    const li=document.createElement("li")
-    li.innerHTML="<input type='checkbox'>"
-    frame.appendChild(li)
+for(let i=0;i<1000;i++){
+    const tr=document.createElement("tr")
+    for(let j=0;j<1000;j++){
+        const td=document.createElement("td")
+        td.innerHTML="<input type='checkbox'>"
+        tr.appendChild(td)
+    }
+    frame.appendChild(tr)
 }
 ul.appendChild(frame)
