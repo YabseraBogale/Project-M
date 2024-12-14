a2ed7e9458d1e6def54ad8f2bfde85e34d8185ce
YabseraBogale <yabserapython@gmail.com>
2024-12-14 18:33:34 +0300

    ok

--- amazon/main.py
+++ amazon/main.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:114.0) Gecko/20100101 Firefox/114.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
 }
 
-url = "https://www.amazon.com/s?k=laptop"
-response = requests.get(url, headers=headers)
+url = "https://www.amazon.com/s?k=laptops&crid=3BVZ14BC0I8V7&sprefix=laptop%2Caps%2C970&ref=nb_sb_noss_1"
+response = requests.get(url,headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Extract product titles
-titles = [item.text for item in soup.find_all("span", class_="a-size-medium")]
-print(titles)
+
+for i in soup:
+    print(i.get_text())
