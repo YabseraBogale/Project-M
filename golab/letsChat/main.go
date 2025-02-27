09a78f8acee874e5d19a8e57959ff0b824bf9373
YabseraBogale <yabserapython@gmail.com>
2025-02-27 17:28:33 +0300

    ok

--- golab/letsChat/main.go
+++ golab/letsChat/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"io"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -18,16 +16,5 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 }
 
 func main() {
-	e := echo.New()
-	render := &Template{
-		template: template.Must(template.ParseGlob("public/index.html")),
-	}
-	e.Renderer = render
-	e.POST("/", func(c echo.Context) error {
-		name := c.FormValue("message")
-		fmt.Println(name)
-		return c.Render(http.StatusOK, "index.html", nil)
-	})
 
-	e.Logger.Fatal(e.Start("127.0.0.1:8080"))
 }
